6d10163482fcf88309be569f9fbdd4268cc27a4f
Belmonte <belmonte.matheus@gmail.com>
2020-03-24 20:20:08 -0300

    teste

--- includes/connection.php
+++ includes/connection.php
@@ -4,7 +4,7 @@
 
 		public function dbConnect() {
 			//localhost:
-			return new PDO("mysql:host=localhost; dbname=Banco", "Banco","Senha",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+			return new PDO("mysql:host=localhost; dbname=banco_novus", "root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		}
 
 	}
