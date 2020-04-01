f09b8a699e39172d1b193072c886ad6b442badf7
Marcelo <marcelomab09@gmail.com>
2020-04-01 19:05:08 -0300

    atualizando o email

--- email/sendEmail.php
+++ email/sendEmail.php
@@ -1,11 +1,50 @@
+
 <?php 
 	
-	//include_once('connection.php');
-
-	
-
-			// send email
-			mail("mateusdotto@gmail.com","My Test","Mensagem");
-			
-	
-?>
+	include_once('../includes/connection.php');
+	include_once("../includes/tableCadMaq.php");
+  
+	$maquina = new CadMaq();
+	$maqList = $maquina->getCadMaqManutencao();
+  
+  if(!empty($maqList)){
+  
+	  foreach($maqList->maq as $value){
+  
+			  
+			  // emails para quem será enviado o formulário
+			  $emailenviar = "marcelomab09@gmail.com";
+			  $destino = $value['EnderecoEmailAviso'];
+			  $assunto = "Manutenção de maquina ".  $value['EnderecoEmailAviso'];
+			  $msg = "A maquina ".$value['Nome']." irá ter uma manutenção em ". $value['AvisoAntesDays'] . " dias ";
+		 
+  
+  
+			  
+			  // É necessário indicar que o formato do e-mail é html
+			  
+			  $headers  = 'MIME-Version: 1.0' . "\r\n";
+			  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+			  $headers .= 'From: marcelomab09@gmail.com';
+			  //$headers .= "Bcc: $EmailPadrao\r\n";
+			  
+			  $enviaremail = mail($destino, $assunto, $msg, $headers);
+			  if($enviaremail){
+				 echo "email enviado com sucesso";
+			  }else{
+				  echo "deu erro";
+			  
+			  }
+			  // $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+			  // echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+			  // } else {
+			  // $mgm = "ERRO AO ENVIAR E-MAIL!";
+			  // echo "";
+			  // }
+					  
+	  
+	  }
+  }
+			  
+	  
+  ?>
