181561e110c74bf71d9e2e3b8934d4c72b2ec653
Marcelo <marcelomab09@gmail.com>
2020-05-25 08:50:37 -0300

    criando a modal

--- MaquinaEdit.php
+++ MaquinaEdit.php
@@ -157,38 +157,42 @@
 				  </div>
 
 				  <div class="form-group row">
-				    <label for="input2" class="col-2 col-form-label required">Período de Manutenção (em dias)</label>
+				   <!--  <label for="input2" class="col-2 col-form-label required">Período de Manutenção (em dias)</label>
 				    <div class="col-sm-10">
 						<input type="number" required
-							<?php if ($Id <> 0 && !$IsEdit) {echo 'readonly';} ?> 
+							<?php /* if ($Id <> 0 && !$IsEdit) {echo 'readonly';}  */?> 
 							class="form-control customInputForm" name='periodoManutencao' id="input5" 
 							value=<?php 
-										if ($Id <> 0) {
+										/* if ($Id <> 0) {
 											echo '"'.$resp->MaqDados["Período de Manutenção (em dias)"].'"'; 
 										} else {
 											echo '""'; 
-										}
-									?>
+										} */
+									?>s
 						>
-				    </div>
-				  </div>
+				    </div> -->
+					<!-- Botão que irá abrir o modal -->
+					<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#meuModal">Agendamento de manutenção</button>	
 
-				  <div class="form-group row">
-				    <label for="input2" class="col-2 col-form-label required">Tempo, em dias, antes de mandar email de aviso</label>
-				    <div class="col-sm-10">
-						<input type="number" required
-							<?php if ($Id <> 0 && !$IsEdit) {echo 'readonly';} ?> 
-							class="form-control customInputForm" name='avisoAntes' id="input6" 
-							value=<?php 
-										if ($Id <> 0) {
-											echo '"'.$resp->MaqDados["Tempo, em dias, antes de mandar email de aviso"].'"'; 
-										} else {
-											echo '""'; 
-										}
-									?>
-						>
-				    </div>
-				  </div>
+				  </div> 
+				
+
+					 <div class="form-group row">
+						<label for="input2" class="col-2 col-form-label required">Tempo, em dias, antes de mandar email de aviso</label>
+						<div class="col-sm-10">
+							<input type="number" required
+								<?php if ($Id <> 0 && !$IsEdit) {echo 'readonly';} ?> 
+								class="form-control customInputForm" name='avisoAntes' id="input6" 
+								value=<?php 
+											if ($Id <> 0) {
+												echo '"'.$resp->MaqDados["Tempo, em dias, antes de mandar email de aviso"].'"'; 
+											} else {
+												echo '""'; 
+											}
+										?>
+							>
+						</div>
+				  	</div>
 
 			
 				
@@ -276,7 +280,41 @@
 				  } ?>
 				  <a href="MaquinasList.php" class="btn btn-secondary" style="margin-bottom: 10px;">Voltar</a>
 				</form>
-				
+
+			   	<!-- Optional JavaScript -->
+				<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+				<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+				<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+				<!-- Modal -->
+				<div id="meuModal" class="modal fade" role="dialog">
+				<div class="modal-dialog">
+			
+					<!-- Conteúdo do modal-->
+					<div class="modal-content">
+			
+					<!-- Cabeçalho do modal -->
+					<div class="modal-header">
+						<h4 class="modal-title">Título do modal</h4>
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>
+			
+					<!-- Corpo do modal -->
+					<div class="modal-body">
+						<p>Conteúdo no corpo do modal.</p>
+						<p>Aqui você pode inserir todo o conteúdo necessário.</p>
+					</div>
+			
+					<!-- Rodapé do modal-->
+					<div class="modal-footer">
+						<button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+						<button type="button" class="btn btn-primary">Salvar Alterações</button>
+					</div>
+			
+					</div>
+				</div>
+				</div>
+			
 			</div>
 	</main>
 <?php 
