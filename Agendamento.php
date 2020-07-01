e9a82a192518c25a44ab4a0797f8aa489137899c
Marcelo <marcelomab09@gmail.com>
2020-07-01 17:33:09 -0300

    retirando o agendamento da modal

--- Agendamento.php
+++ Agendamento.php
@@ -1,2 +1,68 @@
 <?php 
- echo $_POST['DataInicial'];
+if(isset($_POST['DataInicial'])){
+    
+
+}
+?>
+<?php 
+	require "header.php";
+?>
+<main id="MainDiv" class="offset-2">
+     <div class="customDiv">
+        <h4 class="modal-title"><b>Hoje é <?php echo date("d/m/Y")?></b>. Faça o seu agendamento de manutenção</h4>
+                       
+        <form action= "./Agendamento.php" method="POST">	
+        
+            <div class="form-group" >
+                <div class="col-sm-auto">
+                <label for="tipoPeriodoId"><b>Apartir de</b></label>
+                    <input type="date" class="form-control customInputForm" name='DataInicial' id="DataInicial" value="" ?>
+            </div>
+               
+        
+            <div class="form-group">
+                <label for="tipoPeriodoId"><b>Período</b></label>
+                <select name="tipoPeriodoId" class="form-control">
+                    <option value="M">Mensal</option>
+                    <option value="T">Trimestral</option>
+                    <option value="S">Semestral</option>
+                    <option value="A">Anual</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="Quantidade">Qtd:</label>
+                <input type="number" id="quantity" name="QuantidadePeriodo" min="1" max="12">
+            </div>
+
+            <div class="form-group">
+                <label for="tipoSemanaId"><b>Recorrência da Semana</b></label>
+                <select    name="recorrencia">
+                    <option value="0">Semanalmente</option>
+                    <option value="1">1ª Semana</option>
+                    <option value="2">2ª Semana</option>
+                    <option value="3">3ª Semana</option>
+                    <option value="4">4ª Semana</option>
+                </select>
+            </div>
+            
+            <div class = "form-row">
+                <label for="tipoSemanaId"><b>Toda a:</b></label>
+                <select    name="diaSemana">
+                    <option value="seg">Segunda-feira</option>
+                    <option value="ter">Terça-feira</option>
+                    <option value="Qua">Quarta-feira</option>
+                    <option value="Qui">Quinta-feira</option>
+                    <option value="Sex">Sexta-feira</option>
+                    <option value="Sab">Sexta-feira</option>
+                    <option value="Dom">Sexta-feira</option>
+                </select>
+                
+            </div>
+
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Voltar</button>
+            <button type="submit" class="btn btn-primary">Salvar Alterações</button>
+       
+        </form>
+     </div> 
+</main>
+
--- MaquinaEdit.php
+++ MaquinaEdit.php
@@ -221,7 +221,7 @@
 					  }
 				  ?>
 				  	<?php if (!empty($_GET['Id'])||!isset($_GET['Id'])) { ?>
-						<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#meuModal">Agendamento da Manutenção</button>	
+						<a href="Agendamento.php" class="btn btn-secondary" style="margin-bottom: 10px;">Agendamento de manutenção</a>	
 					<?php } ?> 
 
 				  <?php 
@@ -282,90 +282,7 @@
 				  <a href="MaquinasList.php" class="btn btn-secondary" style="margin-bottom: 10px;">Voltar</a>
 				</form>
 
-			   	<!-- Optional JavaScript -->
-				<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-				<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-				<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-				<!-- Modal -->
-				<div id="meuModal" class="modal fade" role="dialog">
-					<div class="modal-dialog">
 			
-					<!-- Conteúdo do modal-->
-						<div class="modal-content">
-				
-						<!-- Cabeçalho do modal -->
-							<div class="modal-header">
-								<h4 class="modal-title"><b>Hoje é <?php echo date("d/m/Y")?></b>. Faça o seu agendamento de manutenção</h4>
-								<button type="button" class="close" data-dismiss="modal">&times;</button>
-							</div>
-				
-						<!-- Corpo do modal -->
-						<div class="modal-body">
-							<div class="container-fluid">
-								<div class="row">
-									<form action= "./Agendamento.php" method="POST">	
-										<div class="col-sm-9">
-										<div class="form-group row" name="Periodos">
-											<div class="col-sm-auto">
-												<b>A partir de</b> 
-										</div>
-											<input type="date" class="form-control customInputForm" name='DataInicial' id="DataInicial" value="" ?>
-										</div>
-										<div class="form-group">
-											<label for="tipoPeriodoId"><b>Período</b></label>
-											<select name="tipoPeriodoId" class="form-control">
-												<option value="M">Mensal</option>
-												<option value="T">Trimestral</option>
-												<option value="S">Semestral</option>
-												<option value="A">Anual</option>
-											</select>
-										</div>
-										<div class="form-group">
-
-											<label for="Quantidade">Qtd:</label>
-											
-											<input type="number" id="quantity" name="QuantidadePeriodo" min="1" max="12">
-												
-										</div>
-											<div class = "form-row">
-												<label for="tipoSemanaId"><b>Recorrência da Semana</b></label>
-												<select    name="recorrencia">
-													<option value="0">Semanalmente</option>
-													<option value="1">1ª Semana</option>
-													<option value="2">2ª Semana</option>
-													<option value="3">3ª Semana</option>
-													<option value="4">4ª Semana</option>
-												</select>
-											</div>
-											
-
-										</div>
-											<div class="col-sm-auto" name="tipoDiaSemanaId">
-											<div class = "form-row">
-													<label for="tipoSemanaId"><b>Toda a:</b></label>
-													<select    name="diaSemana">
-														<option value="seg">Segunda-feira</option>
-														<option value="ter">Terça-feira</option>
-														<option value="Qua">Quarta-feira</option>
-														<option value="Qui">Quinta-feira</option>
-														<option value="Sex">Sexta-feira</option>
-														<option value="Sab">Sexta-feira</option>
-														<option value="Dom">Sexta-feira</option>
-													</select>
-												</div>
-									</div>
-							
-									<!-- Rodapé do modal-->
-									<div class="modal-footer">
-										<button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
-										<button type="button" class="btn btn-primary">Salvar Alterações</button>
-									</div>
-								</form>
-							</div>	
-						</div>
-					</div>
-				</div>
 			
 			</div>
 	</main>
